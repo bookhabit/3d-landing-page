b94ad6e680c6d9fa40e65ff5d6a3c9c27a972952
bookhabit <qnrgoqlt@naver.com>
2024-03-22 00:39:28 +0900

    Hero section - video설정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { GlobalStyle } from './styles/GlobalStyle';
 import Quote from './section/Quote';
+import HeroSection from './section/HeroSection';
 
 function App() {
   return (
     <>
       <GlobalStyle/> 
       <Quote/>
+      <HeroSection/>
     </>
   );
 }
